e8d246eb298fa60886db6d0e8143b595f7c4b34a
Am4deuss <isak.treptow@hotmail.se>
2024-09-22 21:31:31 +0200

    commented topmost.py

--- lab1/topmost.py
+++ lab1/topmost.py
@@ -4,19 +4,19 @@ import importlib.util
 import urllib.request
 import wordfreq
 
-def readFile():
+def readFile(): # function that reads file if the 2nd argument is a textfile
     file = open(sys.argv[2], encoding="utf-8")
     readData = file.read()
     lines = readData.split("\n")
     file.close()
     return lines
 
-def main():
+def main(): # main program function
 
-    with open(sys.argv[1], encoding="utf-8") as inp_file:
-        stopWords = wordfreq.tokenize(inp_file)
+    with open(sys.argv[1], encoding="utf-8") as inp_file: # built-in fuction which opens the stopfile (file is specified as the 1st argument when launching program)
+        stopWords = wordfreq.tokenize(inp_file) # produces list of words to ignore in count
 
-    if sys.argv[2].startswith("https://") or sys.argv[2].startswith("http://"):
+    if sys.argv[2].startswith("https://") or sys.argv[2].startswith("http://"): # checks if the 2nd argument is a textfile or website. if website -> read website
         response = urllib.request.urlopen(sys.argv[2])
         lines = response.read().decode("utf8").splitlines()
     else:
@@ -24,6 +24,7 @@ def main():
 
     n = int(sys.argv[3])
 
+    # explained in wordfreq.py
     words = wordfreq.tokenize(lines)
     wordCount = wordfreq.countWords(words, stopWords)
     wordfreq.printTopMost(wordCount, n)
